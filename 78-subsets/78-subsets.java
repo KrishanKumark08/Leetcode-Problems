a9162680947a5daad1aaa1260f5f68fbe282d4cc
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-02-13 22:42:04 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (24.65%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> powerSet = new ArrayList<>();
-        
-        generateAllSubsets(nums, 0, powerSet, new ArrayList<>());
-        
-        return powerSet;
+        List<List<Integer>> arr=new ArrayList<>();
+        subset(nums,0,arr,new ArrayList<>());
+        return arr;
     }
-    public void generateAllSubsets(int[] nums, int currentIndex, List<List<Integer>> powerSet, List<Integer> currentSubset)
+    public void subset(int[] nums,int n,List<List<Integer>> arr,List<Integer> list)
     {
-        if(currentIndex >= nums.length)
+        if(n==nums.length)
         {
-            powerSet.add(new ArrayList<>(currentSubset));
+            arr.add(new ArrayList<>(list));
+           //  for(List<Integer> r:arr)
+           //  {
+           // for(int i:r)
+           //     System.out.print(i+" ");
+           //  System.out.println();
+           //  }
             return ;
         }
-        
-        int currentVal = nums[currentIndex];
-        currentSubset.add(nums[currentIndex]);
-        
-        generateAllSubsets(nums, currentIndex+1, powerSet, currentSubset);
-        
-        currentSubset.remove(currentSubset.size()-1);
-        generateAllSubsets(nums, currentIndex+1, powerSet, currentSubset);
+       // ArrayList<Integer> list2=new ArrayList<>(list);
+        list.add(nums[n]);
+        subset(nums,n+1,arr,list);
+        list.remove(list.size()-1);
+        subset(nums,n+1,arr,list);
     }
 }
