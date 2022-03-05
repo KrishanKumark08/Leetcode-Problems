037174de66db2e32ce821d5d0917a956e07a3742
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-03-05 22:30:06 +0530

    Added solution - LeetHub

---
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j]  =Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortest_distance(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j  = 0; j < n; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        int n = matrix.length;
+        int[][] costs = new int[n][n];
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == -1){
+                    costs[i][j] = Integer.MAX_VALUE;
+                }
+                else{
+                    costs[i][j] = matrix[i][j];
+                }
+            }
+        }
+        
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(costs[i][k] != Integer.MAX_VALUE && costs[k][j] != Integer.MAX_VALUE){
+                        costs[i][j] = Math.min(costs[i][j], costs[i][k] + costs[k][j]);
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(costs[i][j] == Integer.MAX_VALUE){
+                    matrix[i][j] = -1;
+                }
+                else{
+                    matrix[i][j] = costs[i][j];
+                }
+            }
+        }
+        return;
+    }
+}
