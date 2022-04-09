edca1c6893ba7d8e1d945e08bc470fb2f370d18b
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-04-09 12:44:56 +0530

    Time: 2 ms (75.15%), Space: 41.9 MB (27.60%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,25 +1,20 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> list = new ArrayList<Integer>();
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
+        
         for(int stone:stones){
-            list.add(stone);
+            pQueue.offer(stone);
         }
-        Collections.sort(list);
-        return func(list);
-    }
-    public int func(List<Integer> list)
-    {
-        if(list.size()==1)
-            return list.get(0);
-        if(list.size()==0)
-            return 0;
-        int diff = list.get(list.size()-1) - list.get(list.size()-2);
-            list.remove(list.size()-1);
-            list.remove(list.size()-1);
-            if(diff!=0){
-                list.add(diff);
-                Collections.sort(list);
-            }
-        return func(list);
+        
+        while(pQueue.size() > 1){
+            int x = pQueue.poll();
+            int y = pQueue.poll();
+            
+            pQueue.offer(Math.abs(x - y));
+            
+        }
+        
+        return pQueue.peek();
+        
     }
 }
