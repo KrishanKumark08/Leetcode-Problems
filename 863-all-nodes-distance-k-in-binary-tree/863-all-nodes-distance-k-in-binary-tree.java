9d043acd8faa5f3f954816d677f49b2f8660488b
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-04-16 09:30:15 +0530

    Time: 10 ms (90.71%), Space: 42.2 MB (88.66%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        HashMap<TreeNode, TreeNode> childParent = new HashMap<>();
+        
+        childAndParent(root, childParent, null);
+        
+        List<Integer> answer = new ArrayList<>();
+        nodesAtDistanceK(target, k, new HashSet<>(), answer, childParent);
+        return answer;
+    }
+    
+    private void childAndParent(TreeNode root, HashMap<TreeNode, TreeNode> childParent, TreeNode parent){
+        if(root == null){
+            return;
+        }
+        
+        childParent.put(root, parent);
+        
+        childAndParent(root.left, childParent, root);
+        childAndParent(root.right, childParent, root);
+        
+    }
+    
+    private void nodesAtDistanceK(TreeNode root, int k, HashSet<TreeNode> set, List<Integer> answer, HashMap<TreeNode, TreeNode> childParent){
+        
+        if(root == null || set.contains(root)){
+            return;
+        }
+        
+        if(k == 0){
+            answer.add(root.val);
+            return;
+        }
+        
+        set.add(root);
+        nodesAtDistanceK(root.left, k - 1, set, answer, childParent);
+        nodesAtDistanceK(root.right, k - 1, set, answer, childParent);
+        nodesAtDistanceK(childParent.get(root), k - 1, set, answer, childParent);
+        set.remove(root);
+        
+    }
+}
