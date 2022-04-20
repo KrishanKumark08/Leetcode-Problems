5876e38c6d804a99d62345114c73a073ca404d2e
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-04-20 17:43:52 +0530

    Time: 7 ms (9.72%), Space: 49.9 MB (5.92%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        HashMap<TreeNode, TreeNode> parentMap = new HashMap<>();
+        HashMap<Integer, TreeNode> nodeMap = new HashMap<>();
+        populateMap(root, parentMap, null, nodeMap);
+        
+        List<TreeNode> ans = new ArrayList<>();
+        ans.add(root);
+        
+        HashSet<TreeNode> set = new HashSet<>();
+        for(int i : to_delete){
+            set.add(nodeMap.get(i));
+        }
+        
+        for(int i:to_delete){
+            TreeNode currNode = nodeMap.get(i);
+            if(currNode == root){
+                ans.remove(root);
+                if(currNode.left != null){
+                if(!set.contains(currNode.left))
+                    ans.add(currNode.left);
+            }
+            
+            if(currNode.right != null){
+                if(!set.contains(currNode.right))
+                    ans.add(currNode.right);
+            }
+                continue;
+            }
+            if(currNode.left != null){
+                if(!set.contains(currNode.left))
+                    ans.add(currNode.left);
+            }
+            
+            if(currNode.right != null){
+                if(!set.contains(currNode.right))
+                    ans.add(currNode.right);
+            }
+            
+            if(currNode != root){
+                    TreeNode parent = parentMap.get(currNode);
+                
+                if(parent.left == currNode){
+                parent.left = null;
+            }
+            else{
+                parent.right = null;
+            }
+                
+            }
+            
+            
+        }
+        return ans;
+    }
+    
+    private void populateMap(TreeNode root, HashMap<TreeNode, TreeNode> memo, TreeNode parent, HashMap<Integer, TreeNode> nodeMap){
+        if(root == null){
+            return;
+        }
+        
+        memo.put(root, parent);
+        nodeMap.put(root.val, root);
+        populateMap(root.left, memo, root, nodeMap);
+        populateMap(root.right, memo, root, nodeMap);
+        
+    }
+    
+}
