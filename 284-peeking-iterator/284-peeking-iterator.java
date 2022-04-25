5762ddc40b122e5f5c52068c8712ffe282cad0bd
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-04-25 20:51:06 +0530

    Time: 7 ms (39.41%), Space: 42.9 MB (42.94%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -0,0 +1,37 @@
+import java.util.NoSuchElementException;
+class PeekingIterator implements Iterator<Integer> {
+    Integer next;
+    Iterator<Integer> iter;
+    boolean noSuchElement;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+	iter = iterator;
+        advanceIter();
+    }
+
+    public Integer peek() {
+          return next;
+    }
+
+    @Override
+    public Integer next() {
+        if (noSuchElement)
+            throw new NoSuchElementException();
+        Integer res = next;
+        advanceIter();
+        return res;
+    }
+
+    @Override 
+    public boolean hasNext() {
+        return !noSuchElement;
+    }
+    
+    private void advanceIter() {
+        if (iter.hasNext()) {
+            next = iter.next();
+        } else {
+            noSuchElement = true;
+        }
+    }
+}
