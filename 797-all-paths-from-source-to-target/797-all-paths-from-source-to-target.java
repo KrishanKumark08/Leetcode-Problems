5ac3cc02b0a46ac83001b628651cc14b03f77fc8
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-04-30 10:02:56 +0530

    Time: 12 ms (13.19%), Space: 55.5 MB (24.69%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> currAns = new ArrayList<>();
-        allPaths(graph, 0, graph.length - 1, currAns, ans);
+        Queue<List<Integer>> queue = new LinkedList<>();
+        
+        List<Integer> curList = new ArrayList<>();
+        curList.add(0);
+        
+        queue.add(curList);
+        
+        while(!queue.isEmpty()){
+            curList = queue.remove();
+            
+            int v = curList.get(curList.size() - 1);
+            
+            if(v == graph.length - 1){
+                ans.add(curList);
+                continue;
+            }
+            
+            for(int neighbour:graph[v]){
+                List<Integer> newList = new ArrayList<>(curList);
+                newList.add(neighbour);
+                queue.add(newList);
+            }
+            
+        }
         return ans;
     }
-    
-    private void allPaths(int[][] graph, int src, int target, List<Integer> currAns, List<List<Integer>> ans){
-        if(src == target){
-            currAns.add(src);
-            ans.add(new ArrayList<>(currAns));
-            currAns.remove(currAns.size() - 1);
-            return;
-        }
-        
-        currAns.add(src);
-        
-        for(int i:graph[src]){
-            allPaths(graph, i, target, currAns, ans);
-        }
-        currAns.remove(currAns.size() - 1);
-        return;
-    }
-    
 }
