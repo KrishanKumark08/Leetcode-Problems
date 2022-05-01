27461636daaefa836b3b80af74794b1977faccef
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-01 21:34:20 +0530

    Added solution - LeetHub

--- Negative weight cycle - GFG/negative-weight-cycle.java
+++ Negative weight cycle - GFG/negative-weight-cycle.java
@@ -36,9 +36,9 @@ class Solution
 {
     public int isNegativeWeightCycle(int n, int[][] edges)
     {
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        int dist[] = new int[n];
         
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
         
         for(int i = 0; i < n - 1; i++){
@@ -47,9 +47,8 @@ class Solution
                 int dest = edge[1];
                 int weight = edge[2];
                 
-                if(dist[src] != Integer.MAX_VALUE){
+                if(dist[src] != Integer.MAX_VALUE)
                     dist[dest] = Math.min(dist[dest], dist[src] + weight);
-                }
                 
             }
         }
@@ -59,11 +58,9 @@ class Solution
                 int dest = edge[1];
                 int weight = edge[2];
                 
-                if(dist[src] != Integer.MAX_VALUE && dist[dest] > dist[src] + weight){
+                if(dist[src] != Integer.MAX_VALUE && dist[dest] > dist[src] + weight)
                     return 1;
-                }
             }
-        
         return 0;
     }
 }
