e343a0902a76d9d2eb29e5d9a9aff37d029d12c6
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-05 21:19:15 +0530

    Time: 13 ms (11.38%), Space: 54.7 MB (67.04%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        // BFS
         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> currList = new ArrayList<>();
+        currList.add(0);
+        
         Queue<List<Integer>> queue = new LinkedList<>();
-        
-        List<Integer> curList = new ArrayList<>();
-        curList.add(0);
-        
-        queue.add(curList);
+        queue.add(currList);
         
         while(!queue.isEmpty()){
-            curList = queue.remove();
+            currList = queue.remove();
             
-            int v = curList.get(curList.size() - 1);
+            int v = currList.get(currList.size() - 1);
             
             if(v == graph.length - 1){
-                ans.add(curList);
+                ans.add(currList);
                 continue;
             }
             
             for(int neighbour:graph[v]){
-                List<Integer> newList = new ArrayList<>(curList);
+                List<Integer> newList = new ArrayList<>(currList);
                 newList.add(neighbour);
                 queue.add(newList);
             }
-            
         }
         return ans;
     }
