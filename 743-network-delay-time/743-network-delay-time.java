00fb08741db978d0228a5f3f0430e53bb8c31094
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-14 07:09:20 +0530

    Time: 43 ms (21.86%), Space: 47.2 MB (75.37%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,48 +1,77 @@
-class Pair implements Comparable<Pair>{
+class Pair implements Comparable<Pair> {
     int vertex;
     int cost;
-    Pair(int vertex, int cost){
+    Pair(int vertex , int cost)
+    {
         this.vertex = vertex;
         this.cost = cost;
     }
     
     public int compareTo(Pair o){
-        return this.cost - o.cost;
+        return this.cost-o.cost;
     }
-    
 }
 
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
+    public int networkDelayTime(int[][] times, int V, int S) {
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
+        
+        S= S;
+        for(int i =0; i<=V ; i++)
+            adj.add(new ArrayList<>());
+        for(int[] i : times)
+        {
+            ArrayList<ArrayList<Integer>> arr = adj.get(i[0]);
+            
+            ArrayList <Integer> list = new ArrayList<>();
+            list.add(i[1]);
+            list.add(i[2]);
+            
+            arr.add(list);
+        }
+        
+        for(ArrayList<ArrayList<Integer>> o : adj)
+        System.out.println(o);
+        PriorityQueue <Pair> pq = new PriorityQueue<>();
+        int costs[] = new int[V+1];
+        
+        pq.add(new Pair(S,0));
+        
         HashSet<Integer> visited = new HashSet<>();
         
-        pq.add(new Pair(k, 0));
-        
-        int ans = -1;
-        
-        while(!pq.isEmpty()){
-            Pair currentPair = pq.remove();
-            int currentVertex = currentPair.vertex;
-            int currentCost = currentPair.cost;
+        int count = 0;
+        int max = 0;
+        while(pq.size() > 0){
+            Pair cn = pq.remove();
             
-            if(visited.contains(currentVertex))
-                continue;
+            int cc = cn.cost;
+            int current = cn.vertex;
             
-            visited.add(currentVertex);
-            ans = Math.max(ans, currentCost);
+            if(visited.contains(current))
+            continue;
             
-            for(int i = 0; i < times.length; i++){
-                if(times[i][0] == currentVertex){
-                    if(!visited.contains(times[i][1])){
-                        pq.add(new Pair(times[i][1], times[i][2] + currentCost));
-                    }
-                }
+            visited.add(current);
+            costs[current] = cc;
+            max = Math.max(max , cc);
+            count++;
+            for(ArrayList<Integer> i : adj.get(current))
+            {
+                if(!visited.contains(i.get(0)))
+                pq.add(new Pair(i.get(0) , cc+i.get(1)));
             }
+            
         }
-        if(visited.size() == n){
-            return ans;
-        }
+        
+        // int count = 0;
+        // for(int i : costs)
+        // {
+        //     if(i == 0)
+        //         count++;
+        // }
+        
+        if(count == V)
+        return max;
+        
         return -1;
     }
 }
