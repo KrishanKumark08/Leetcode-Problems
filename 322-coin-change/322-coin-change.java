e15403fd832bee19ec13bfd369145b3973aff24d
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-21 07:22:11 +0530

    Time: 38 ms (31.72%), Space: 58.2 MB (11.05%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,34 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int ans = minCoins(coins, 0, amount, new HashMap<String,Integer>());
-        
-        if(ans == 100000){
+        int h=coinchange(coins,amount);
+        if(h==100000)
             return -1;
-        }
-        
-        return ans;
+        return h;
     }
-    public int minCoins(int[] coins, int currentIndex, int amount, HashMap<String,Integer> memo){
-        if(amount==0)
-            return 0;
-        if(currentIndex >= coins.length)
-            return 100000; //Maximum Value of Amount in constraints
-        
-        String currentKey = Integer.toString(currentIndex) + "_" + Integer.toString(amount);
-        
-        if(memo.containsKey(currentKey)){
-            return memo.get(currentKey);
+    private int coinchange(int[] coins,int amount)
+    {
+        int[][] dp= new int[coins.length+1][amount+1];
+        for(int i=0;i<coins.length+1;i++)
+        {
+            for(int j =0;j<amount+1;j++)
+            {
+                if(j == 0)
+                    dp[i][j]=0;
+                if(i == coins.length)
+                    dp[i][j]=100000;
+            }
         }
-        
-        int consider = 100000; // 
-        if(coins[currentIndex] <= amount){
-            consider = 1 + minCoins(coins, currentIndex, amount - coins[currentIndex], memo);
+       for(int i=coins.length-1;i>=0;i--)
+        {
+            for(int j =1;j<amount+1;j++)
+            {
+                if(j-coins[i]>=0)
+                      dp[i][j]=Math.min(1+dp[i][j-coins[i]] ,dp[i+1][j]) ;
+                else
+                    dp[i][j] = dp[i+1][j];
+            }
         }
-        
-        int notConsider = minCoins(coins, currentIndex + 1, amount, memo);
-        
-        memo.put(currentKey, Math.min(consider, notConsider));
-        
-        return memo.get(currentKey); 
+        return dp[0][amount];
     }
 }
