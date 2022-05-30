0e5a84e162346d13fbbf0ba8713cdfb3ba9c5a3c
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-30 14:43:57 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (8.28%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,48 +15,49 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null)
+        if(root == null){
+            return null;
+        }
+        
+        else if(root.val > key){
+            root.left = deleteNode(root.left, key);
             return root;
-        
-        if(root.val<key)
-            root.right = deleteNode(root.right,key);
-        
-        else if(root.val>key)
-            root.left = deleteNode(root.left,key);
-        
-        else
-        {
-           if(root.left==null && root.right==null)
-           {
-               root = null;
-               return root;
-           }
-            if(root.left==null)
-            {
+        }
+        else if(root.val < key){
+            root.right = deleteNode(root.right, key);
+            return root;
+        }
+        else{
+            if(root.left == null && root.right == null){
+                root = null;
+                return root;
+            }
+            if(root.left != null && root.right == null){
+                root = root.left;
+                return root;
+            }
+            else if(root.right != null && root.left == null){
                 root = root.right;
                 return root;
             }
-            
-           if(root.right==null)
-            {
-              root = root.left;
-                return root;
-            }
-            
-            if(root.left!=null && root.right!=null)
-            {
-                TreeNode temp = root.right;
-                while(temp.left!=null)
-                {
-                    temp = temp.left;
-                }
-                int h = root.val;
-                root.val=temp.val;
-                temp.val = h;
-                root.right = deleteNode(root.right,temp.val);
+            else{
+                TreeNode tempNode = findLeftMostNode(root.right);
+                
+                int temp = root.val;
+                root.val = tempNode.val;
+                tempNode.val = temp;
+                
+                root.right = deleteNode(root.right, tempNode.val);
                 return root;
             }
         }
-        return root;
+        
+    }
+    
+    private TreeNode findLeftMostNode(TreeNode currentNode){
+        while(currentNode.left != null){
+            currentNode = currentNode.left;
+        }
+        return currentNode;
     }
 }
