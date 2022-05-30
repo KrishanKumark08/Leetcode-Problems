9f1b99ded9041c174382903080e32d5af32dd5ed
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-05-30 14:44:13 +0530

    Time: 12 ms (14.08%), Space: 61.2 MB (37.86%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -15,52 +15,45 @@
  */
 class Solution {
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> ans = inorderTraversal(root);
-        int[] arr = new int[ans.size()];
-        int j =0;
-        for(int i:ans)
-        {
-            arr[j] = i;
-            j++;
-        }
-       return sortedArrayToBST(arr);
-    }
-     public List<Integer> inorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        List<Integer> ans = new ArrayList<>();
-        if(root==null)
-            return ans;
-        int count =1;
-        while(count==1)
-        {
-            while(root!=null )
-            {
-                stack.push(root);
-                root = root.left;
-            }
-            if(stack.size()==0)
-                break;
-            root = stack.pop();
-            ans.add(root.val);
-            root = root.right;
-        }
-        return ans;
-    }
-     public TreeNode sortedArrayToBST(int[] nums) {
-        if(nums.length==1)
-            return new TreeNode(nums[0]);
-        TreeNode root = new TreeNode(0);
-        return arraytobst(nums,0,nums.length-1,(0+nums.length-1)/2,root);
-          
-    }
-    private TreeNode arraytobst(int[] nums,int l ,int r,int mid,TreeNode root)
-    {
-        if(l>r)
-            return null;
+        List<Integer> inOrder = new ArrayList<>();
         
-        root = new TreeNode(nums[mid]);
-        root.left = arraytobst(nums,l ,mid-1,(l+mid-1)/2,root.left);
-        root.right = arraytobst(nums,mid+1 ,r,(r+mid+1)/2,root.right);
-        return root;
+        inorder(root, inOrder);
+        
+        int[] nums = new int[inOrder.size()];
+        for(int i = 0; i < inOrder.size(); i++){
+            nums[i] = inOrder.get(i);
+        }
+        return constructBSTFromSortedArray(nums, 0, nums.length - 1);
     }
+    
+    private TreeNode constructBSTFromSortedArray(int[] nums, int left, int right){
+        
+        if(left > right ){
+            return null;
+        }
+        
+        int mid = (left + right)/2;
+        
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = constructBSTFromSortedArray(nums, left, mid - 1);
+        root.right = constructBSTFromSortedArray(nums, mid + 1, right);
+        
+        return root;
+        
+    }
+    
+    private void inorder(TreeNode root, List<Integer> answer){
+        
+        if(root == null){
+            return;
+        }
+        
+        inorder(root.left, answer);
+        answer.add(root.val);
+        inorder(root.right, answer);
+        
+        return;
+        
+    }
+    
 }
