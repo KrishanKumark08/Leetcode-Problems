0da588779b758456930708f7602fb84d46d3a5d3
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-03 15:29:07 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (20.16%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.java
@@ -15,72 +15,40 @@
  */
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-      List<TreeNode> deepestLeaves = new ArrayList<>();
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        
-        while(!queue.isEmpty()){
-            
-            int size = queue.size(); // It represents the number of nodes present at any Level.
-            List<TreeNode> currentAns = new ArrayList<>();
-            
-            while(size > 0){
-                TreeNode currentNode = queue.remove();
-                currentAns.add(currentNode);
-                
-                if(currentNode.left != null){
-                    queue.add(currentNode.left);
-                }
-                
-                if(currentNode.right != null){
-                    queue.add(currentNode.right);
-                }
-                size--;
-            }
-            deepestLeaves = currentAns;
-        }
-        
-        
-        if(deepestLeaves.size() == 1){
-            return deepestLeaves.get(0);
-        }
-        
-        for(int i = 1; i < deepestLeaves.size(); i++){
-            TreeNode ans = lowestCommonAncestor(root, deepestLeaves.get(i - 1), deepestLeaves.get(i));
-            deepestLeaves.set(i, ans);
-        }
-        
-        return deepestLeaves.get(deepestLeaves.size() - 1);
-        
+    int l = heightofbt(root);
+        return lca(root,l);
     }
-    
-    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
+    public int heightofbt(TreeNode root)
+    {
+        if(root==null)
+            return 0;
+        return 1 + Math.max(heightofbt(root.left) , heightofbt(root.right));
+    }
+    public TreeNode lca(TreeNode root, int l)
+    {
+        if(root==null)
+            return null;
+        if(root.right == null && root.left == null)
+        {
+            if(l==1)
+            {
+              
+                return root;
+            } 
             return null;
         }
         
-        if(root.val == p.val || root.val == q.val){
+        TreeNode right = lca(root.right,l-1);
+        TreeNode left = lca(root.left,l-1);
+        if(left != null && right != null  ) 
             return root;
-        }
         
-        TreeNode leftAns = lowestCommonAncestor(root.left, p, q);
-        TreeNode rightAns = lowestCommonAncestor(root.right, p, q);
+         if(right != null ) 
+            return right;
         
-        if(leftAns != null && rightAns != null){
-            return root;
-        }
-        
-        if(leftAns != null){
-            return leftAns;
-        }
-        
-        if(rightAns != null){
-            return rightAns;
-        }
+        if(left != null ) 
+            return left;
         
         return null;
-        
     }
-    
 }
