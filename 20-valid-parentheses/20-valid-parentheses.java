95999699f13b569655808a8922f02799fc62c1a9
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-06 21:01:28 +0530

    Time: 4 ms (23.01%), Space: 42.4 MB (22.86%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -4,31 +4,27 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(c == '(' || c == '{' || c == '['){
-                stack.push(c);
+            if(c == '}' || c == ']' || c == ')'){
+                if(stack.empty()){
+                    return false;
+                }
+                else{
+                    if(c == '}' && stack.peek() == '{')
+                        stack.pop();
+                    else if(c == ']' && stack.peek() == '[')
+                        stack.pop();
+                    else if(c == ')' && stack.peek() == '(')
+                        stack.pop();
+                    else
+                        return false;
+                }
             }
             else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                char top = stack.pop();
-                if(c == ')' && top != '('){
-                    return false;
-                }
-                else if(c == '}' && top != '{'){
-                    return false;
-                }
-                else if(c == ']' && top != '['){
-                    return false;
-                }
+                stack.push(c);
             }
         }
-        
-        if(!stack.isEmpty()){
-            return false;
-        }
-        
-        return true;
+    
+        return stack.empty();
         
     }
 }
