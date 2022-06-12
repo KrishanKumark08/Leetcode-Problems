790ebdf6f4e601c0fc7caa269eb2e55eb922c140
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-12 16:35:59 +0530

    Added solution - LeetHub

--- Floor in a Sorted Array - GFG/floor-in-a-sorted-array.java
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.java
@@ -41,6 +41,8 @@ class Solution{
         int start = 0;
         int end = n - 1;
         
+        int ans = -1;
+        
         while(start <= end){
             int mid = start + (end - start)/2;
             if(nums[mid] == target){
@@ -48,13 +50,14 @@ class Solution{
             }
             
             if(nums[mid] < target){
+                ans = mid;
                 start = mid + 1;
             }
             else{
                 end = mid - 1;
             }
         }
-        return end;
+        return ans;
     }
     
 }
