63d01ce65844f099220c4b2cb5e58cb4747928b8
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-20 19:16:02 +0530

    Time: 4 ms (55.45%), Space: 44.5 MB (40.17%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,37 +14,28 @@
  * }
  */
 class Solution {
-    int postIndex;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        postIndex = inorder.length - 1;
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        
+    HashMap<Integer, Integer> inorderHashMap = new HashMap<>();
         for(int i = 0; i < inorder.length; i++){
-            memo.put(inorder[i], i);
+            inorderHashMap.put(inorder[i], i);
         }
-        
-        return constructTree(postorder, inorder, 0, postorder.length - 1, memo);
+        int arr[] = new int[1];
+        arr[0] = inorder.length-1;
+        return construct(postorder,arr,inorderHashMap,0,inorder.length-1);
     }
     
-    private TreeNode constructTree(int[] postorder, int[] inorder, int left, int right, HashMap<Integer, Integer> memo){
-        
-        if(left > right){
+    private TreeNode construct(int[] postorder,int[] arr,HashMap <Integer,Integer> inorderHashMap,int l,int r)
+    {
+        if(l>r)
             return null;
-        }
         
-        int currVal = postorder[postIndex];
-        postIndex--;
-        
-        TreeNode root = new TreeNode(currVal);
-        
-        int i = -1;
-        i = memo.get(currVal);
-        
-        root.right = constructTree(postorder, inorder, i + 1, right, memo);
-        root.left = constructTree(postorder, inorder, left, i - 1, memo);
-        
+        TreeNode root = new TreeNode(postorder[arr[0]]);
+        int index = inorderHashMap.get(postorder[arr[0]]);
+        arr[0] = arr[0] - 1;
+        root.right = construct(postorder,arr,inorderHashMap,index+1,r);
+         root.left = construct(postorder,arr,inorderHashMap,l,index-1);
         
         return root;
-        
     }
 }
+   
