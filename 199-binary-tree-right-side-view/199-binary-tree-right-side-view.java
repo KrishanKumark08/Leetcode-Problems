538423487dad08a57b7227826e466396a91c1dac
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-23 10:03:01 +0530

    Time: 2 ms (42.68%), Space: 43 MB (32.34%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -15,29 +15,27 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        
-        rightView(root, memo, 0);
-        
-        for(int key:memo.keySet()){
-            answer.add(memo.get(key));
+        List<Integer> list= new ArrayList<>();
+        if(root==null)
+        {
+            return list;
         }
-        
-        return answer;
+       // list.add(root.val);
+        rightview(root,list,new HashSet<>(),0);
+        return list;
     }
-    
-    private void rightView(TreeNode root, HashMap<Integer, Integer> memo, int level){
-        
-        if(root == null){
+    public void rightview(TreeNode root,List<Integer> list,HashSet<Integer>set,int level)
+    {
+        if(root==null)
             return;
-        }
+       if(!set.contains(level))
+       {
+           set.add(level);
+           list.add(root.val);
+       }
         
-        memo.put(level, root.val);
-        
-        rightView(root.left, memo, level + 1);
-        rightView(root.right, memo, level + 1);
-        
+         rightview(root.right,list,set,level+1);
+         rightview(root.left,list,set,level+1);
+        return;
     }
-    
 }
