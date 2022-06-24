a6a3552840062b83a5aeec43d4c785d389038c7a
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-24 12:36:26 +0530

    Time: 6 ms (58.97%), Space: 46.1 MB (21.92%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        combinations(candidates,target,0,new ArrayList<>(),list);
-        return list;
+        List<List<Integer>> ans = new ArrayList<>();
+        generateAllCombinations(candidates, 0, target, new ArrayList<>(), ans);
+        return ans;
     }
-    public void combinations(int[] nums, int target,int n,ArrayList<Integer>arr,
-                             List<List<Integer>> list)
-    {
-        if(target==0)
-        {
-            list.add(new ArrayList(arr));
+    
+    private void generateAllCombinations(int arr[], int currentIndex, int target, List<Integer> currentAns, List<List<Integer>> ans){
+        if(target <= 0 || currentIndex >= arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(currentAns));
+            }
             return;
         }
-        if(n>=nums.length)
-            return;
-        if(nums[n]<=target)
-        {
-            arr.add(nums[n]);
-            combinations(nums,target-nums[n],n,arr,list);
-            arr.remove(arr.size()-1);
-        }
-        combinations(nums,target,n+1,arr,list);
-        return;
+        
+        int currentValue = arr[currentIndex];
+        currentAns.add(currentValue);
+        generateAllCombinations(arr, currentIndex, target - currentValue, currentAns, ans);
+        currentAns.remove(currentAns.size() - 1);
+        generateAllCombinations(arr, currentIndex + 1, target, currentAns, ans);
+        
+        
     }
 }
