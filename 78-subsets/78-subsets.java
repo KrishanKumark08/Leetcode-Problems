f32c37ff05da50ace69f40de9d28bf1ee2024b15
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-24 12:09:21 +0530

    Time: 2 ms (35.16%), Space: 43 MB (73.80%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> arr=new ArrayList<>();
-        subset(nums,0,arr,new ArrayList<>());
-        return arr;
-    }
-    public void subset(int[] nums,int n,List<List<Integer>> arr,List<Integer> list)
-    {
-        if(n==nums.length)
-        {
-            arr.add(new ArrayList<>(list));
-           //  for(List<Integer> r:arr)
-           //  {
-           // for(int i:r)
-           //     System.out.print(i+" ");
-           //  System.out.println();
-           //  }
-            return ;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> currentAns = new ArrayList<>();
+        calcSubsets(nums, 0, currentAns, ans);
+        return ans;
+    } 
+    
+    private void calcSubsets(int nums[], int currentIndex, List<Integer> currentAns, List<List<Integer>> ans){
+        if(currentIndex >= nums.length){
+            ans.add(new ArrayList<>(currentAns));
+            return;
         }
-       // ArrayList<Integer> list2=new ArrayList<>(list);
-        list.add(nums[n]);
-        subset(nums,n+1,arr,list);
-        list.remove(list.size()-1);
-        subset(nums,n+1,arr,list);
+        
+        currentAns.add(nums[currentIndex]);
+        calcSubsets(nums, currentIndex + 1, currentAns, ans);
+        currentAns.remove(currentAns.size() - 1);
+        calcSubsets(nums, currentIndex + 1, currentAns, ans);   
     }
 }
