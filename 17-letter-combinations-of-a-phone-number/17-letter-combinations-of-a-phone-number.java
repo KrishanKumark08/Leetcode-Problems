1b84fb634faba4a302e0e977a9bca6cd83f5d312
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-25 21:00:35 +0530

    Time: 10 ms (14.95%), Space: 43.3 MB (16.30%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,46 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-		LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
-	}
+        List<String> answer = new ArrayList<String>();
+        
+        if(digits.length() == 0){
+            return answer;
+        }
+        
+        HashMap<Character, String> memo = new HashMap<>(){{
+            put('2',"abc");
+            put('3',"def");
+            put('4',"ghi");
+            put('5',"jkl");
+            put('6',"mno");
+            put('7',"pqrs");
+            put('8',"tuv");
+            put('9',"wxyz");
+            
+        }};
+        
+        generateAllCombinations(digits, 0, "", answer, memo);
+        
+        return answer;
+        
+    }
+    
+    private void generateAllCombinations(String digits, int currentIndex, String currentAns, List<String> answer, HashMap<Character, String> memo){
+        
+        if(currentIndex == digits.length()){
+            answer.add(currentAns);
+            return;
+        }
+        
+        char currentChar = digits.charAt(currentIndex);
+        
+        String currentString = memo.get(currentChar);
+        
+        for(int i = 0; i < currentString.length(); i++){
+            generateAllCombinations(digits, currentIndex + 1, currentAns + currentString.charAt(i), answer, memo);
+        }
+        
+        return;
+        
+    }
+    
 }
