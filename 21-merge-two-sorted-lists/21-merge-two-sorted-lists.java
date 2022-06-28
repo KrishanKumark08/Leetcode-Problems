a8b843f5c949c01ceff45863a5340b3a341fbc31
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-06-28 12:25:18 +0530

    Time: 1 ms (76.58%), Space: 43.6 MB (11.99%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,39 +10,45 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummynode=new ListNode(-1);
-         ListNode h=dummynode;
-        while(list1!=null&&list2!=null)
-        {
-            if(list1.val>=list2.val)
-            {
-                ListNode n=new ListNode(list2.val);
-                dummynode.next=n;
-                dummynode=dummynode.next;
-                list2=list2.next;
+        if(list1 == null){
+            return list2;
+        }
+        else if(list2 == null){
+            return list1;
+        }
+        
+        ListNode dummyNode = new ListNode(-101);
+        ListNode head = dummyNode;
+        while(list1 != null && list2!= null){
+            if(list1.val <= list2.val){
+                ListNode newNode = new ListNode(list1.val);
+                list1 = list1.next;
+                dummyNode.next = newNode;
+                dummyNode = dummyNode.next;
             }
-            else
-            {
-               ListNode n=new ListNode(list1.val);
-                dummynode.next=n;
-                dummynode=dummynode.next;
-                list1=list1.next; 
+            else{
+                ListNode newNode = new ListNode(list2.val);
+                list2 = list2.next;
+                dummyNode.next = newNode;
+                dummyNode = dummyNode.next;
             }
         }
-        while(list1!=null)
-        {
-          ListNode n=new ListNode(list1.val);
-                dummynode.next=n;
-                dummynode=dummynode.next;
-                list1=list1.next;  
+        
+        while(list1 != null){
+            ListNode newNode = new ListNode(list1.val);
+            list1 = list1.next;
+            dummyNode.next = newNode;
+            dummyNode = dummyNode.next;
         }
-        while(list2!=null)
-        {
-            ListNode n=new ListNode(list2.val);
-                dummynode.next=n;
-                dummynode=dummynode.next;
-                list2=list2.next; 
+        
+        while(list2 != null){
+            ListNode newNode = new ListNode(list2.val);
+            list2 = list2.next;
+            dummyNode.next = newNode;
+            dummyNode = dummyNode.next;
         }
-        return h.next;
+    
+        return head.next;
+    
     }
 }
