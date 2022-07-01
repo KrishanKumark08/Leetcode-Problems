bc59c6b7c6b553eae7b5a7118279b0847d8c0205
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-01 21:56:43 +0530

    Time: 14 ms (37.25%), Space: 42.4 MB (82.45%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = 0; i < dp.length; i++){
             for(int j = 0; j <= i; j++){
                 String wordToCheck = s.substring(j, i + 1);
-                System.out.println(wordToCheck);
                 if(wordDict.contains(wordToCheck)){
                     if(j > 0)
                         dp[i] += dp[j - 1];
