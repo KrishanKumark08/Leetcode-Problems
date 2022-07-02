4c342aea1d52d467630b4f6c665c3baa8142ebe2
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-02 13:33:26 +0530

    Time: 2 ms (6.45%), Space: 41.8 MB (76.48%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,65 +13,22 @@
  *     }
  * }
  */
-
-class Pair{
-    TreeNode node;
-    int count;
-    Pair(){}
-    Pair(TreeNode node, int count){
-        this.node = node;
-        this.count = count;
-    }
-}
-
 class Solution {
-    
-    // Calculating all the traversals in 1 GO
-    // TC : O(3*n)
-    // SC : O(4*n)
     public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
         
-        List<Integer> preorder = new ArrayList<>();
-        List<Integer> inorder = new ArrayList<>();
-        List<Integer> postorder = new ArrayList<>();
-        
-        if(root == null){
-            return postorder;
-        }
-        
-        Stack<Pair> stack = new Stack<>();
-        
-        Pair pair= new Pair(root, 1);
-        stack.push(pair);
+        if(root != null)
+            stack.push(root);
         
         while(!stack.isEmpty()){
-            Pair currPair= stack.pop();
-            
-            if(currPair.count == 1){
-                preorder.add(currPair.node.val);
-                currPair.count++;
-                stack.push(currPair);
-                if(currPair.node.left != null){
-                    stack.push(new Pair(currPair.node.left, 1));
-                }
-            }
-            else if(currPair.count == 2){
-                inorder.add(currPair.node.val);
-                currPair.count++;
-                stack.push(currPair);
-                if(currPair.node.right != null){
-                    stack.push(new Pair(currPair.node.right, 1));
-                }
-            }
-            else if(currPair.count == 3){
-                postorder.add(currPair.node.val);
-            }
+            TreeNode currentNode = stack.pop();
+            ans.add(currentNode.val);
+            if(currentNode.left != null) stack.push(currentNode.left);
+            if(currentNode.right != null) stack.push(currentNode.right);
         }
         
-        System.out.println(preorder);
-        System.out.println(inorder);
-        System.out.println(postorder);
-        
-        return postorder;
+        Collections.reverse(ans);
+        return ans;
     }
 }
