22dde2146b23f574597903cfb0d073d763f800b6
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-13 14:49:12 +0530

    Time: 1 ms (53.06%), Space: 40.8 MB (63.70%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,30 +1,52 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return totalPaths(m, n, 0, 0, new HashMap<>());
+        
+        int[][] memo = new int[m][n];
+        
+        memo[m-1][n-1] = 1;
+        
+        for(int i = m - 1; i >=0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1){
+                    continue;
+                }
+                if(i + 1 < m){
+                    memo[i][j] += memo[i + 1][j];
+                }
+                
+                if(j + 1 < n){
+                    memo[i][j] += memo[i][j+1];
+                }
+            }
+        }
+        
+        return memo[0][0];        
+        
+        //return totalPaths(m, n, 0, 0, new HashMap<>());
     }
     
-    private int totalPaths(int row, int col, int currentRow, int currentCol, HashMap<String, Integer> memo){
-        if(currentRow >= row || currentCol >= col){
-            return 0;
-        }
+//     private int totalPaths(int row, int col, int currentRow, int currentCol, HashMap<String, Integer> memo){
+//         if(currentRow >= row || currentCol >= col){
+//             return 0;
+//         }
         
-        if(currentRow == row - 1 && currentCol == col - 1){
-            return 1;
-        }
+//         if(currentRow == row - 1 && currentCol == col - 1){
+//             return 1;
+//         }
         
-        String currentKey = currentRow + "_" + currentCol;
+//         String currentKey = currentRow + "_" + currentCol;
         
-        if(memo.containsKey(currentKey)){
-            return memo.get(currentKey);
-        }
+//         if(memo.containsKey(currentKey)){
+//             return memo.get(currentKey);
+//         }
         
-        int right = totalPaths(row, col, currentRow, currentCol + 1, memo);
-        int down = totalPaths(row, col, currentRow + 1, currentCol, memo);
+//         int right = totalPaths(row, col, currentRow, currentCol + 1, memo);
+//         int down = totalPaths(row, col, currentRow + 1, currentCol, memo);
         
-        memo.put(currentKey, right + down);
-        return right + down;
+//         memo.put(currentKey, right + down);
+//         return right + down;
         
         
-    }
+//     }
     
 }
