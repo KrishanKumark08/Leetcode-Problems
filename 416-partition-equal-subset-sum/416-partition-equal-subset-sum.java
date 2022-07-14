3ec976b09f254a15131fe567e5e30699f67724a6
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-14 08:56:36 +0530

    Time: 35 ms (80.26%), Space: 43.1 MB (79.43%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -14,28 +14,52 @@ class Solution {
     }
     
     private boolean isPossible(int[] nums, int sum, int currentIndex, HashMap<String, Boolean> memo){
+        int n = nums.length;
+        
         if(sum == 0){
             return true;
         }
         
-        if(currentIndex >= nums.length || sum < 0){
-            return false;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        
+        for(int j = 0; j < sum + 1; j++){
+            dp[n][j] = false;
         }
         
-        String currentKey = currentIndex + "_" + sum;
-        if(memo.containsKey(currentKey)){
-            return memo.get(currentKey);
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = true;
         }
         
-        boolean consider = isPossible(nums, sum - nums[currentIndex], currentIndex + 1, memo);
-        if(consider == true){
-            return true;
+        for(int i = n -1; i >= 0; i--){
+            for(int j = 1; j <= sum; j++){
+                dp[i][j] = dp[i+1][j] || ((j - nums[i] >= 0) ? dp[i + 1][j - nums[i]] : false);
+            }
         }
-        boolean notConsider = isPossible(nums, sum, currentIndex + 1, memo);
+        
+        return dp[0][sum];
+        
+//         if(sum == 0){
+//             return true;
+//         }
+        
+//         if(currentIndex >= nums.length || sum < 0){
+//             return false;
+//         }
+        
+//         String currentKey = currentIndex + "_" + sum;
+//         if(memo.containsKey(currentKey)){
+//             return memo.get(currentKey);
+//         }
+        
+//         boolean consider = isPossible(nums, sum - nums[currentIndex], currentIndex + 1, memo);
+//         if(consider == true){
+//             return true;
+//         }
+//         boolean notConsider = isPossible(nums, sum, currentIndex + 1, memo);
         
         
-        memo.put(currentKey, consider || notConsider);
-        return memo.get(currentKey);
+//         memo.put(currentKey, consider || notConsider);
+//         return memo.get(currentKey);
         
     }
     
