a099310a3d65eb723a7113673f5056733fce984a
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-14 07:22:28 +0530

    Time: 4 ms (64.77%), Space: 44.2 MB (36.19%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,25 +1,44 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        return minPath(0, 0, triangle,new HashMap<String, Integer>());
-    }
-    public int minPath(int currentRow, int currentCol, List<List<Integer>> triangle, HashMap<String, Integer> memo){
-        if(currentRow >= triangle.size() || currentRow >= triangle.get(currentRow).size()){
-            return 0;
-        }
-        if(currentRow == triangle.size() - 1){
-            return triangle.get(currentRow).get(currentCol);   
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
         
-        String currentKey = currentRow + "_" + currentCol;
-        if(memo.containsKey(currentKey))
-            return memo.get(currentKey);
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                int diag = triangle.get(i).get(j) + dp[i + 1][j + 1];
+                
+                dp[i][j] = Math.min(down, diag);
+                
+            }
+        }
         
-        int i = triangle.get(currentRow).get(currentCol) + minPath(currentRow + 1, currentCol, triangle, memo);
+        return dp[0][0];
         
-        int iPlusOne = triangle.get(currentRow).get(currentCol) + minPath(currentRow + 1, currentCol + 1, triangle, memo);
-        
-        memo.put(currentKey, Math.min(i, iPlusOne));
-        return memo.get(currentKey);
-        
+        //return minPath(0, 0, triangle,new HashMap<String, Integer>());
     }
+//     public int minPath(int currentRow, int currentCol, List<List<Integer>> triangle, HashMap<String, Integer> memo){
+//         if(currentRow >= triangle.size() || currentRow >= triangle.get(currentRow).size()){
+//             return 0;
+//         }
+//         if(currentRow == triangle.size() - 1){
+//             return triangle.get(currentRow).get(currentCol);   
+//         }
+        
+//         String currentKey = currentRow + "_" + currentCol;
+//         if(memo.containsKey(currentKey))
+//             return memo.get(currentKey);
+        
+//         int i = triangle.get(currentRow).get(currentCol) + minPath(currentRow + 1, currentCol, triangle, memo);
+        
+//         int iPlusOne = triangle.get(currentRow).get(currentCol) + minPath(currentRow + 1, currentCol + 1, triangle, memo);
+        
+//         memo.put(currentKey, Math.min(i, iPlusOne));
+//         return memo.get(currentKey);
+        
+//     }
 }
