38c5c1db026c01d11300d4c680cfe92cb559cd48
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-16 11:01:40 +0530

    Time: 12 ms (94.59%), Space: 46.1 MB (77.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,19 +6,18 @@ class Solution {
         
         int[][] memo = new int[n + 1][m + 1];
         
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = m - 1; j >= 0; j--){
-                //System.out.println("i = "+ i + " " + "j = " + j);
-                if(text1.charAt(i) == text2.charAt(j)){
-                    memo[i][j] = 1 + memo[i + 1][j + 1];
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    memo[i][j] = 1 + memo[i - 1][j - 1];
                 }
                 else{
-                    memo[i][j] = Math.max(memo[i][j + 1], memo[i + 1][j]);
+                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j - 1]);
                 }
             }
         }
         
-        return memo[0][0];
+        return memo[n][m];
         
     }
 }
