8fbd2416eee8f6d50511335bc0cc71bc9f35b670
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-17 10:19:34 +0530

    Time: 28 ms (56.51%), Space: 50.4 MB (37.12%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,7 +1,31 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        return totalWays(s, t, 0, 0, new HashMap<>());
+        int cs = s.length();
+        int ct = t.length();
+        int[][] memo = new int[cs + 1][ct + 1];
+        for(int j = 0; j <= ct; j++){
+            memo[cs][j] = 0;
+        }
+        
+        for(int i = 0; i <= cs; i++){
+            memo[i][ct] = 1;
+        }
+        
+        for(int i = cs - 1; i >= 0; i--){
+            for(int j = ct - 1; j >= 0; j--){
+                if(s.charAt(i) == t.charAt(j)){
+                    memo[i][j] = memo[i + 1][j + 1] + memo[i + 1][j];
+                }
+                else{
+                    memo[i][j] = memo[i + 1][j];
+                }
+            }
+        }
+        
+        return memo[0][0];
+        
     }
+    
     
     private int totalWays(String s, String t, int cs, int ct, HashMap<String, Integer> memo){
         if(ct >= t.length()){
