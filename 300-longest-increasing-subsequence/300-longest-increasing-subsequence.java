534816f4d70f0b2030d28c586ad663475bfa5289
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-20 13:09:11 +0530

    Time: 116 ms (16.13%), Space: 45.6 MB (12.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,46 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        return longestincsubs(nums);
-    }
-    public int longestincsubs(int[] nums)
-    {
-        int dp[][] = new int[nums.length+1][nums.length+1];
-        for(int current = nums.length-1; current>=0 ; current--){
-            
-            for(int prev = current - 1; prev>=-1 ; prev--){
-                
-                int consider = 0;
-                if(prev == -1 || nums[current] > nums[prev])
-                    consider = 1 + dp[current+1][current+1];
-                
-                int notconsider =  dp[current+1][prev+1];
-                dp[current][prev+1] = Math.max(consider , notconsider);
-            }
+        int n = nums.length;
+        int[] dp = new int[n];
+        
+        for(int i = 0;i < n; i++){
+            dp[i] = 1;
         }
         
-        return dp[0][0];
+        int hash[] = new int[n];
+        int lastIndex = 0;
+        
+        int answer = 1;
+        
+        for(int i = 1;i < n; i++){
+            hash[i] = i;
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j] && 1 + dp[j] > dp[i]){
+                    dp[i] = 1 + dp[i];
+                    hash[i] = j;
+                }
+                
+                if(dp[i] > answer){
+                    answer = dp[i];
+                    lastIndex = i;
+                }
+            } 
+        }
+        // Printing LIS
+        int arr[] = new int[answer];
+        arr[0] = nums[lastIndex];
+        int index = 1;
+        
+        while(hash[lastIndex] != lastIndex){
+            lastIndex = hash[lastIndex];
+            arr[index++] = nums[lastIndex];
+        }
+        
+        for(int i = 0; i < answer; i++){
+            System.out.print(arr[i] + " ");
+        }
+        
+        
+        return answer;
     }
 }
