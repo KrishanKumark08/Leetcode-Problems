1b3646eec5de342cf7f359384903b16feb790ff8
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-20 13:48:07 +0530

    Time: 88 ms (26.83%), Space: 44.7 MB (82.39%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,17 +1,50 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Map<String, Integer> dp = new HashMap<>();
         Arrays.sort(words, (a, b)->a.length() - b.length());
-        int res = 0;
-        for (String word : words) {
-            int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+        // Because Here we can pick any sequence 
+        // Here it is not subsequence
+        // so if we sort it 
+        // Take this example for reference ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+        
+        
+        int n = words.length;
+        int dp[] = new int[n];
+        int maxi = 1;
+        
+        Arrays.fill(dp, 1);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(checkPossible(words[i], words[j]) && 1 + dp[j] > dp[i]){
+                    dp[i] = 1 + dp[j];
+                }
             }
-            dp.put(word, best);
-            res = Math.max(res, best);
+            maxi = Math.max(maxi, dp[i]);
         }
-        return res;
+        
+        return maxi;
     }
+    
+    private boolean checkPossible(String s1, String s2){
+        if(s1.length() != s2.length() + 1){
+            return false;
+        }
+        
+        int first = 0;
+        int second = 0;
+        
+        while(first < s1.length()){
+            if(second < s2.length() && s1.charAt(first) == s2.charAt(second)){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        
+        return first == s1.length() && second == s2.length();
+        
+    }
+    
 }
