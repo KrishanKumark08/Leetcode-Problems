e9bee3c9e39b0dc50a52795dd151aab94e2fbaa8
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-25 08:57:58 +0530

    Time: 19 ms (18.83%), Space: 55 MB (70.84%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+        stack1.push(intervals[0][0]);
+        stack2.push(intervals[0][1]);
+        
+        for(int i = 1; i < intervals.length; i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            if(start >= stack1.peek() && start <= stack2.peek()){
+                if(end <= stack2.peek()){
+                    continue;
+                }
+                stack2.pop();
+                stack2.push(end);
+            }
+            else{
+                stack1.push(start);
+                stack2.push(end);
+            }
+            
+            // if(stack1.peek() > start && end > stack2.peek()){
+            //     stack1.pop();
+            //     stack2.pop();
+            //     stack1.push(start);
+            //     stack2.push(end);
+            // }
+            // else if(stack1.peek() < start && end < stack2.peek()){
+            //     continue;
+            // }
+            // else if(stack1.peek() <= start && start <= stack2.peek()){
+            //     stack2.pop();
+            //     stack2.push(end);
+            // }
+            // else if(start < stack1.peek() && end< stack1.peek()){
+            //     stack1.push(start);
+            //     stack2.push(end);
+            // }
+            // else if(start <= stack1.peek() && end<= stack2.peek()){
+            //     stack1.pop();
+            //     stack1.push(start);
+            // }
+            // else{
+            //     stack1.push(start);
+            //     stack2.push(end);
+            // }
+        }
+        
+        int n = stack1.size();
+        int[][] arr = new int[n][2];
+        
+        for(int i = n - 1; i >= 0; i--){
+            arr[i][0] = stack1.pop();
+            arr[i][1] = stack2.pop();
+        }
+        
+        return arr;
+        
+    }
+}
