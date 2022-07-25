69fcc05e6c743316d65bb36370ccf96ff516fe0c
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-25 09:53:04 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (31.68%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int fib(int n) {
-        return nthFib(n,new HashMap<Integer,Integer>());
-    }
-    public int nthFib(int n,HashMap<Integer,Integer> memo)
-    {
-        if(n<=1)
+        if(n == 0 || n== 1){
             return n;
+        }
         
-        if(memo.containsKey(n))
-            return memo.get(n);
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
         
-        memo.put(n,nthFib(n-1,memo) + nthFib(n-2,memo));
+        for(int i = 2; i <= n; i++){
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
         
-        return memo.get(n);
+        return memo[n];
+        
     }
 }
