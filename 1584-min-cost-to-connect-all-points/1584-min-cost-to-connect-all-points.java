beaca8eab1e4a00fa3d8540c629f618b434c1cf3
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-07-29 13:37:51 +0530

    Time: 2116 ms (5.00%), Space: 184.7 MB (11.46%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,39 +1,57 @@
+class Pair implements Comparable<Pair>{
+    int x;
+    int y;
+    int cost;
+    Pair(int x, int y, int cost){
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+    }
+    
+    public int compareTo(Pair o){
+        return this.cost - o.cost;
+    }
+    
+}
+
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
+        HashSet<String> visited = new HashSet<>();
         
-        PriorityQueue<Pair<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> (a.getKey() - b.getKey()));;
+        int totalCost = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
         
-        boolean[] inMST = new boolean[n];
+        int startX = points[0][0];
+        int startY = points[0][1];
+        pq.add(new Pair(startX, startY, 0));
         
-        heap.add(new Pair(0, 0));
-        int mstCost = 0;
-        int edgesUsed = 0;
-        
-        while (edgesUsed < n) {
-            Pair<Integer, Integer> topElement = heap.poll();
+        while(!pq.isEmpty()){
+            Pair currentPair = pq.remove();
+            int x = currentPair.x;
+            int y = currentPair.y;
+            int currentCost = currentPair.cost;
             
-            int weight = topElement.getKey();
-            int currNode = topElement.getValue();
-            
-            if (inMST[currNode]) {
+            String str = x + "_" + y;
+            if(visited.contains(str)){
                 continue;
             }
             
-            inMST[currNode] = true;
-            mstCost += weight;
-            edgesUsed++;
+            totalCost += currentCost;
+            visited.add(str);
             
-            for (int nextNode = 0; nextNode < n; ++nextNode) {
-                if (!inMST[nextNode]) {
-                    int nextWeight = Math.abs(points[currNode][0] - points[nextNode][0]) + 
-                                     Math.abs(points[currNode][1] - points[nextNode][1]);
-        
-                    heap.add(new Pair(nextWeight, nextNode));
+            for(int[] point:points){
+                int x1 = point[0];
+                int y1 = point[1];
+                
+                String st = x1 + "_" + y1;
+                if(visited.contains(st)){
+                    continue;
                 }
-            }
+                
+                pq.add(new Pair(x1, y1, Math.abs(x - x1) + Math.abs(y - y1)));
+                
+            }  
         }
-        
-        return mstCost;
+        return totalCost;
     }
 }
