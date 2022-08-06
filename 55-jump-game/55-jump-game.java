2abc3d0e6fded73b51be5c990850d009158543ef
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-08-06 10:11:22 +0530

    Time: 1 ms (100.00%), Space: 68 MB (49.21%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,31 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        return isPossible(nums, 0, new HashMap<Integer, Boolean>());
-    }
-    public boolean isPossible(int[] nums, int currentIndex, HashMap<Integer,Boolean> memo){
-        if(currentIndex >= nums.length - 1)
-            return true;
+        int goal = nums.length - 1;
         
-        int currentJump = nums[currentIndex];
-        
-        if(currentJump >= nums.length - currentIndex)
-            return true;
-        
-        int currentKey = currentIndex;
-        
-        if(memo.containsKey(currentKey))
-            return memo.get(currentKey);
-        
-        boolean ans = false;
-        
-        for(int i=1; i<=currentJump; i++){
-            boolean tempAns = isPossible(nums, currentIndex + i, memo);
-            ans = ans || tempAns;
-            // if(tempAns == true)
-            //     break;
+        for(int i = nums.length - 1; i>= 0; i--){
+            if(i + nums[i] >= goal){
+                goal = i;
+            }
         }
         
-        memo.put(currentKey,ans);
-           return ans;
+        return goal == 0;
+        
     }
 }
