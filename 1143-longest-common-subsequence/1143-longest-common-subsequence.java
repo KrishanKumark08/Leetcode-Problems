75824d374a805a005da83d39bee5101cb5b3e454
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-08-13 21:20:25 +0530

    Time: 16 ms (80.49%), Space: 46.9 MB (16.60%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,26 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        return solve(text1, text2, 0, 0, new int[text1.length()][text2.length()]);
-    }
-    
-    private int solve(String s1, String s2,int cI1, int cI2, int[][] memo){
-        if(cI1 >= s1.length() || cI2 >= s2.length()){
-            return 0;
+        
+        int m = text1.length();
+        int n = text2.length();
+        
+        int[][] memo = new int[m + 1][n + 1];
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    memo[i][j] = 1 + memo[i + 1][j + 1];
+                }
+                else{
+                    memo[i][j] = Math.max(memo[i + 1][j], memo[i][j + 1]);
+                }
+            }
         }
         
-        if(memo[cI1][cI2] != 0){
-            return memo[cI1][cI2];
-        }
-        
-        
-        if(s1.charAt(cI1) == s2.charAt(cI2)){
-            memo[cI1][cI2] = 1 + solve(s1, s2, cI1 + 1, cI2 + 1, memo);
-            return memo[cI1][cI2];
-        }
-        
-        memo[cI1][cI2] = Math.max(solve(s1, s2, cI1 + 1, cI2, memo), solve(s1, s2, cI1, cI2 + 1, memo));
-        return memo[cI1][cI2];
+        return memo[0][0];
         
     }
-    
 }
