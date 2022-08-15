03eeef3ea8f3b983655081a5ce5b0349dbc7ee32
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-08-15 10:16:26 +0530

    Time: 48 ms (18.14%), Space: 73.3 MB (37.21%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        int numberOfConnectedComponents = 0;
+        
+        HashSet<Integer> visited = new HashSet<>();
+        
+        List<List<Integer>> graph = createGraph(n, connections);
+        
+        for(int i = 0; i < n; i++){
+            if(!visited.contains(i)){
+                numberOfConnectedComponents++;
+                runDFS(i, visited, graph);
+            }
+        }
+        
+//         if(numberOfConnectedComponents == 1){
+//             return -1;
+//         }
+        if(connections.length >= n - 1)
+            return numberOfConnectedComponents - 1;
+        
+        return -1;
+        
+        
+    }
+
+    private void runDFS(int currentVertex, HashSet<Integer> visited, List<List<Integer>> graph){
+        if(visited.contains(currentVertex)){
+            return;
+        }
+        
+        visited.add(currentVertex);
+        for(int neigh : graph.get(currentVertex)){
+            runDFS(neigh, visited, graph);
+        }
+        
+    }
+    
+    private List<List<Integer>> createGraph(int n, int[][] connections){
+        List<List<Integer>> graph = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : connections){
+            int u = edge[0];
+            int v = edge[1];
+            
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        
+        return graph;
+        
+    }
+    
+}
