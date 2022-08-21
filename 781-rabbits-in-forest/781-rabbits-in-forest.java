d1d0f84af74a4daf2b96a597ec02131f928fc38d
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-08-21 11:39:20 +0530

    Time: 5 ms (47.10%), Space: 43 MB (30.50%) - LeetHub

---
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRabbits(int[] answers) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        
+        for(int ans : answers){
+            memo.put(ans, memo.getOrDefault(ans, 0) + 1);
+        }
+        
+        int count = 0;
+        for(int key:memo.keySet()){
+            if(memo.get(key) > key){
+                count += ((int)Math.ceil((double)memo.get(key) / (double)(key + 1))) * (key + 1);
+            }
+            else{
+                count += key + 1;
+            }
+        }
+        return count;
+    }
+}
