440109021457f772b42d6efcc70fb9fd1537fae2
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-08-28 15:25:33 +0530

    Time: 18 ms (21.54%), Space: 46.9 MB (47.18%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,20 +1,32 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int[] dp = new int[s.length()];
-        
-        for(int i = 0; i < dp.length; i++){
-            for(int j = 0; j <= i; j++){
-                String wordToCheck = s.substring(j, i + 1);
-                if(wordDict.contains(wordToCheck)){
-                    if(j > 0)
-                        dp[i] += dp[j - 1];
-                    else
-                        dp[i] = 1;
-                }
-            }
+        HashSet<String> set = new HashSet<>();
+        for(String str : wordDict){
+            set.add(str);
+        }
+        return isPossible(s, set, new HashMap<String, Boolean>());
+    }
+    
+    private boolean isPossible(String s, HashSet<String> set, HashMap<String, Boolean> memo){
+        if(set.contains(s)){
+            return true;
         }
         
-        return dp[s.length() - 1] > 0;
+        if(memo.containsKey(s)){
+            return memo.get(s);
+        }
         
+        boolean ans = false;
+        
+        for(int i = 0; i < s.length(); i++){
+            String sub = s.substring(0, i + 1);
+            //System.out.println(sub);
+            if(set.contains(sub)){
+                ans = ans || isPossible(s.substring(i + 1), set, memo);
+            }
+        }
+        memo.put(s, ans);
+        return ans;
     }
+    
 }
