fcf66e9289e9be2c4ae4c390c33de59ad71f62ca
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-10-08 11:27:03 +0530

    Time: 87 ms (5.10%), Space: 54.4 MB (5.07%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,46 +1,36 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        if(word1.length() == 0) return word2.length();
-        if(word2.length() == 0) return word1.length();
-        
-        char[] c1 = word1.toCharArray();
-        char[] c2 = word2.toCharArray();
-        
-        int[][] memo = new int[c1.length][c2.length];
-        
-        for(int i = 0; i < c1.length; i++){
-            for(int j = 0; j < c2.length; j++){
-                memo[i][j] = -1;
-            }
-        }
-        
-        return minOperations(c1, c2, memo, 0, 0);
-        
+        return minOperations(word1, word2, 0, 0, new HashMap<String, Integer>());
     }
     
-    private int minOperations(char[] c1, char[] c2, int[][] memo, int i, int j){
-        if(c1.length == i) return c2.length - j;
-        if(c2.length == j) return c1.length - i;
+    private int minOperations(String s1, String s2, int i1, int i2, HashMap<String, Integer> memo){
         
-        if(memo[i][j] != -1){
-            return memo[i][j];
+        if(i1 == s1.length()){
+            return s2.length() - i2;
         }
         
-        if(c1[i] == c2[j]){
-            memo[i][j] = minOperations(c1, c2, memo, i + 1, j + 1);
+        if(i2 == s2.length()){
+            return s1.length() - i1;
+        }
+        
+        String currKey = i1 + "_" + i2;
+        if(memo.containsKey(currKey)){
+            return memo.get(currKey);
+        }
+        
+        if(s1.charAt(i1) == s2.charAt(i2)){
+            int ans = minOperations(s1, s2, i1 + 1, i2 + 1, memo);
+            memo.put(currKey, ans);
+            return ans;
         }
         else{
-            // insert
-            int insert = minOperations(c1, c2, memo, i, j + 1);
-            
-            int delete = minOperations(c1, c2, memo, i + 1, j);
-            
-            int replace = minOperations(c1, c2, memo, i + 1, j + 1);
-            
-            memo[i][j] = Math.min(insert, Math.min(delete, replace)) + 1;
+            int insert = minOperations(s1, s2, i1, i2 + 1, memo);
+            int delete = minOperations(s1, s2, i1 + 1, i2, memo);
+            int replace = minOperations(s1, s2, i1 + 1, i2 + 1, memo);
+            int ans =  1 + Math.min(insert, Math.min(delete, replace));
+            memo.put(currKey, ans);
+            return ans;
         }
-        
-        return memo[i][j];
         
     }
     
