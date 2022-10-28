bceee246d9da12ce6b1d2653d8a879e6bc0d65c7
Krishan Kumar <52246086+KrishanKumark08@users.noreply.github.com>
2022-10-28 13:19:44 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.util.*;
 import java.io.*;
 import java.lang.*;
@@ -42,31 +42,33 @@ class gfg
 
 
 
-// } Driver Code Ends
 
+// } Driver Code Ends
 
 
 class Solution 
 { 
     //Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[], int n) 
+    static int knapSack(int capacity, int weights[], int values[], int n) 
     { 
-        int[][] dp = new int[n+1][W+1];
+        int memo[][] = new int[n+1][capacity + 1];
         
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < W + 1; j++){
-                if(wt[i - 1] <= j){
-                    dp[i][j] = Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+        for(int j = 0; j < capacity + 1; j++){
+            memo[n][j] = 0;
+        }
+        
+        for(int i = n - 1; i >= 0; i--){
+            for(int weight = 1; weight <= capacity; weight++){
+                int consider = 0;
+                if(weights[i] <= weight){
+                    consider = values[i] + memo[i+1][weight - weights[i]];
                 }
-                else{
-                    dp[i][j] = dp[i - 1][j];
-                }
+                int notConsider = memo[i+1][weight];
+                memo[i][weight] = Math.max(consider, notConsider);
             }
         }
         
-        return dp[n][W];
+        return memo[0][capacity];
         
     } 
 }
-
-
